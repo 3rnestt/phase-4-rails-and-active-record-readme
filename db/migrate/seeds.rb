5fbcbe10729f4b4da98874990f9012289bf13535
Ernest <ernest.wambugu@student.moringaschool.com>
2023-06-22 08:15:00 +0300

    Done

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,4 @@
+# db/seeds.rb
+Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
+Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
+Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
